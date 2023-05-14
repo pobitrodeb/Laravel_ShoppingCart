a2c3e72a1937c75e208a0c3af8a3a78f481debee
Pobitrodeb <pobitrodn@gmail.com>
2023-05-14 21:48:28 +0600

    Add to cart done

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,59 +1,21 @@
-<!doctype html>
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Shooping Cart | www.pobitrodeb.com</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-</head>
+@section('content')
 
-<body>
-    {{-- ########## Header Section ############# --}}
-    <div class="">
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container">
-                <ul class="nav justify-content-end">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Active</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pobitro Deb </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Add To Cart</a>
-                    </li>
-
-                </ul>
+<div class="row">
+    @foreach($products as $product)
+        <div class="col-xs-18 col-sm-6 col-md-3">
+            <div class="thumbnail">
+                <img src="{{ $product->image }}" alt="">
+                <div class="caption">
+                    <h4>{{ $product->name }}</h4>
+                    <p>{{ $product->description }}</p>
+                    <p><strong>Price: </strong> {{ $product->price }}$</p>
+                    <p class="btn-holder"><a href="{{ route('add.to.cart', $product->id) }}" class="btn btn-warning btn-block text-center" role="button">Add to cart</a> </p>
+                </div>
             </div>
-        </nav>
-    </div>
-    {{-- ########## End Header Section ############# --}}
-    <section class="py-5">
-        <div class="container">
-            <div class="row">
-                @forelse ($products as $product)
-                    <div class="col-md-4">
-                        <div class="card m-3">
-                            <img class="card-img-top" src="https://picsum.photos/200" alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $product->name }}</h5>
-                                <p class="card-text">{{ $product->description }}</p>
-                                <a href="#" class="btn btn-warning">Add To Cart </a>
-                            </div>
-                        </div>
-                    </div>
-
-                @empty
-                @endforelse
-            </div>
-
         </div>
-    </section>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-    </script>
-</body>
+    @endforeach
+</div>
 
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::get('/',[ProductController::class, 'index'])->name('product.list');
+Route::get('cart', [ProductController::class, 'cart'])->name('cart');
+Route::get('add-to-cart/{id}', [ProductController::class, 'addToCart'])->name('add.to.cart');
+Route::patch('update-cart', [ProductController::class, 'update'])->name('update.cart');
+Route::delete('remove-from-cart', [ProductController::class, 'remove'])->name('remove.from.cart');
